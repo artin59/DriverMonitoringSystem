b99eefc790d33d05d3ca6a72bc9c77f1363fd7b1
Artin Kiany <artinkiany59@gmail.com>
2026-03-21 20:44:00 -0400

    Wrote motor driver header file

--- Hardware/Driver Monitoring System/include/motorDriver.h
+++ Hardware/Driver Monitoring System/include/motorDriver.h
@@ -1,10 +1,34 @@
 #ifndef MOTORDRIVER_H
 #define MOTORDRIVER_H
 
+#include <stdint.h>
+
+/*
+ * If pin 1 is LOW, and 2 is HIGH:  motor -> forwards
+ * If pin 1 is HIGH, and 2 is LOW:  motor -> backwards
+ * If pin 1 is HIGH, and 2 is HIGH: motor -> stops
+ * If pin 1 is LOW, and 2 is LOW:   motor -> stops
+ */
+
+typedef struct{
+    uint8_t left1;
+    uint8_t left2;
+    uint8_t right1;
+    uint8_t right2;
+} MotorPins;
 
 
+//Initialize the pins by setting them to output
+void initialize(MotorPins *right, MotorPins *left);
 
+//Stops all motors 
+void stop(MotorPins *right, MotorPins *left);
 
+//All motors forwards
+void forwards(MotorPins *right, MotorPins *left);
+
+//All motors backwards
+void backwarsd(MotorPins *right, MotorPins *left);
 
 
 #endif
